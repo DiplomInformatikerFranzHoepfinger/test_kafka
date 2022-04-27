a5f6c9cac1785aa2aef45fefa47cc16f4362a112
Franz Höpfinger <mail@franzhoepfinger.de>
2022-04-27 20:47:55 +0200

    config

---
+++ TestKafka/components/librdkafka/include/config.h
@@ -0,0 +1,20 @@
+/*
+ * config.h
+ *
+ *  Created on: 27.04.2022
+ *      Author: franz
+ */
+
+#ifndef COMPONENTS_LIBRDKAFKA_INCLUDE_CONFIG_H_
+#define COMPONENTS_LIBRDKAFKA_INCLUDE_CONFIG_H_
+
+
+
+
+
+#define DT_LNK      3
+#define BUILT_WITH "XX"
+
+#define HAVE_REGEX 1
+
+#endif /* COMPONENTS_LIBRDKAFKA_INCLUDE_CONFIG_H_ */
