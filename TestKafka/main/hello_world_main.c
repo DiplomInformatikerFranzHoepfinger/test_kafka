388219a383781ef462bcda9fe8f81f4db785eaad
Franz Höpfinger <mail@franzhoepfinger.de>
2022-04-27 20:47:47 +0200

    minimum example.

--- TestKafka/main/hello_world_main.c
+++ TestKafka/main/hello_world_main.c
@@ -2,9 +2,22 @@
 #include <stdio.h>
 #include "sdkconfig.h"
 
+#include "rdkafka.h"
+
 
 void app_main(void)
 {
+
+    rd_kafka_conf_t *conf;   /* Temporary configuration object */
+
+
+    /*
+     * Create Kafka client configuration place-holder
+     */
+    conf = rd_kafka_conf_new();
+
+
+
     printf("Hello world!\n");
 
 }
